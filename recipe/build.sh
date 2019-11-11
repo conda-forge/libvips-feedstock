d52ca8e373d49f16635c0fb47ee691bbb71cbbe9
Sebastian Luna-Valero <sebastian-luna-valero@users.noreply.github.com>
2019-11-11 19:20:02 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 ./configure --prefix="${PREFIX}" 
 make -j${CPU_COUNT}
-make check || (cat tests/test-suite.log && echo "ERROR: make check failed, see above" && exit 1)
+make check || (cat test/test-suite.log && echo "ERROR: make check failed, see above" && exit 1)
 make install
