27c0251e6dade6068e24693519708a350a0b4d60
Isuru Fernando <isuruf@gmail.com>
2021-09-10 08:42:45 -0500

    set PKG_CONFIG

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
+# use pkg-config from BUILD_PREFIX
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+
 ./configure --prefix="${PREFIX}" 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
