3a725a497b09044604f45dc7b4e47814d384929b
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-02-16 16:52:02 +0100

    Build with `-Dauto_features=enabled`

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,17 @@
 set -ex
 
 meson_config_args=(
-    -Dintrospection=enabled
-    -Dopenslide=enabled
+    -Dauto_features=enabled
+    -Dcgif=disabled
+    -Dimagequant=disabled
+    -Djpeg-xl=disabled
+    -Dmatio=disabled
+    -Dnifti=disabled
+    -Dopenexr=disabled
+    -Dpdfium=disabled
+    -Dquantizr=disabled
+    # https://github.com/conda-forge/libvips-feedstock/issues/25
+    -Dspng=disabled
 )
 
 if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
