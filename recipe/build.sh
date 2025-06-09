638cfb3936a971ca5812131162e0201f0aa28ad4
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-06-09 11:16:58 +0200

    Remove redundant iconv workaround
    As it's longer necessary with libarchive 3.8.0.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,27 +1,6 @@
  #!/usr/bin/env bash
 set -ex
 
-# Stopgap while https://github.com/conda-forge/libiconv-feedstock/pull/46
-# it will have a similar file generated
-iconv_pc_created=false
-if [[ -n "${libiconv}" ]] && [[ ! -f ${PREFIX}/lib/pkgconfig/iconv.pc ]]; then
-    iconv_pc_created=true
-    cat > ${PREFIX}/lib/pkgconfig/iconv.pc <<EOF
-prefix=${PREFIX}
-exec_prefix=\${prefix}
-libdir=\${exec_prefix}/lib
-includedir=\${prefix}/include
-Name: iconv
-Description: GNU Unicode conversion library
-Version: ${libiconv}
-Libs: -L\${libdir} -liconv
-Cflags: -I\${includedir}
-EOF
-fi
-
-
-pkg-config --exists --debug libarchive
-
 meson_config_args=(
     -Dauto_features=enabled
     -Dcgif=disabled
@@ -37,20 +16,6 @@ meson_config_args=(
 )
 
 if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
-    if [[ -n "${libiconv}" ]] && [[ ! -f ${BUILD_PREFIX}/lib/pkgconfig/iconv.pc ]]; then
-        cat > ${BUILD_PREFIX}/lib/pkgconfig/iconv.pc <<EOF
-prefix=${BUILD_PREFIX}
-exec_prefix=\${prefix}
-libdir=\${exec_prefix}/lib
-includedir=\${prefix}/include
-Name: iconv
-Description: GNU Unicode conversion library
-Version: ${libiconv}
-Libs: -L\${libdir} -liconv
-Cflags: -I\${includedir}
-EOF
-    fi
-
     unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
     (
         mkdir -p native-build
@@ -113,7 +78,3 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}
 fi
 
 meson install -C build
-
-if [[ "${iconv_pc_created}" == "true" ]]; then
-    rm -f ${PREFIX}/lib/pkgconfig/iconv.pc
-fi
