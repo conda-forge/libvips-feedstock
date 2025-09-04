425bd4cf1ec148776ce4e2b9ba4530ceb66c6e48
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-09-04 16:02:37 +0200

    Add build notes for future libvips 8.18

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,8 @@ meson_config_args=(
     -Dopenexr=disabled
     -Dpdfium=disabled
     -Dquantizr=disabled
+    # Uncomment when libvips 8.18 is released.
+    #-Duhdr=disabled
     # https://github.com/conda-forge/libvips-feedstock/issues/25
     -Dspng=disabled
 )
